659f9dd753526bf10ed1e8978afed57f45dc1745
Tyler Arnold <tylernol@mac.com>
2023-11-22 12:41:07 -0600

    some refactor and missed operands

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -4,7 +4,7 @@
 use linya::{Bar, Progress};
 use std::io::Write;
 use std::fs::OpenOptions;
-
+ use std::fs::File;
 
 
 #[derive(Debug,Clone,Copy)]
@@ -83,7 +83,36 @@ impl std::ops::Add for Vec3 {
         }
     }
 }
+
+impl std::ops::Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        let x = self.x() - other.x();
+        let y = self.y() - other.y();
+        let z = self.z() - other.z();
+
+        Self {
+             e: [x,y,z],
+        }
+    }
+}
+
 //multiply
+impl std::ops::Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let x = self.x() * rhs.x();
+        let y = self.y() * rhs.y();
+        let z = self.z() * rhs.z();
+
+        Self {
+             e: [x,y,z],
+        }
+    }
+}
+
 impl std::ops::Mul<f64> for Vec3 {
     type Output = Self;
 
@@ -97,6 +126,21 @@ impl std::ops::Mul<f64> for Vec3 {
         }
     }
 }
+
+impl std::ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        let x = rhs.x() * self;
+        let y = rhs.y() * self;
+        let z = rhs.z() * self;
+
+        Vec3 {
+             e: [x,y,z],
+        }
+    }
+}
+
 
 impl std::ops::MulAssign<f64> for Vec3 {
     
@@ -152,6 +196,17 @@ pub fn unit_vector(v: &Vec3) -> Vec3 {
     *v / len 
 }
 
+// 3.1 Color Utility Functions
+type Color = Vec3;
+
+pub fn write_color(file: &mut File, c: Color) {
+    let ir = (255.99 * c.x() ) as i32;
+    let ig = (255.99 * c.y() ) as i32;
+    let ib = (255.99 * c.z() ) as i32;
+    file.write_all(format!("{} {} {} \n",ir,ig,ib).as_bytes()).expect("Unable to write data");
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -159,6 +214,32 @@ mod tests {
     fn round_two_digits(x: f64) -> f64 {
         (x * 100.0).round() / 100.0
     }
+
+    #[test]
+    fn test_mul() {
+        let v1 = Vec3::new(1.0,2.0,3.0);
+        let v2 = Vec3::new(2.0,4.0,8.0);
+        let v3 = v1 * v2;
+        assert_eq!(round_two_digits(v3.x()), 2.0);
+        assert_eq!(round_two_digits(v3.y()), 8.0);
+        assert_eq!(round_two_digits(v3.z()), 24.0);
+    }
+
+    #[test]
+    fn test_mul_f64() {
+        let v1 = Vec3::new(1.0,2.0,3.0);
+        let v2 = v1 * 2.0;
+        assert_eq!(round_two_digits(v2.x()), 2.0);
+        assert_eq!(round_two_digits(v2.y()), 4.0);
+        assert_eq!(round_two_digits(v2.z()), 6.0);
+        let v1 = Vec3::new(1.0,2.0,3.0);
+        let v2 = 2.0 * v1;
+        assert_eq!(round_two_digits(v2.x()), 2.0);
+        assert_eq!(round_two_digits(v2.y()), 4.0);
+        assert_eq!(round_two_digits(v2.z()), 6.0);
+
+    }
+
 
     #[test]
     fn test_mul_assign() {
@@ -238,14 +319,13 @@ fn main() {
     for j in 0..image_height {
         progress.set_and_draw(&bar, j);
         for i in 0..image_width {
+            
+
             let r = (i as f64) / ((image_width -1) as f64);
             let g = (j as f64) / ((image_height -1) as f64);
             let b = 0.0;
-            let ir = (255.99 * r ) as i32;
-            let ig = (255.99 * g ) as i32;
-            let ib = (255.99 * b ) as i32;
-            f.write_all(format!("{} {} {} \n",ir,ig,ib).as_bytes()).expect("Unable to write data");
-            //fs::write(file_name,format!("{} {} {}", ir,ig,ib))?;
+            let pixel_color = Color::new(r,g,b);
+            write_color(&mut f,pixel_color);
         }    
     }  
     //Ok(())
